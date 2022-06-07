a065a809d97fb038d102879b1046121cacf5aa18
G. Morris <93727813+v-maxson@users.noreply.github.com>
2022-06-06 22:17:05 -0600

    feat(chip8): implement op_0

--- src/chip8/instructions/op_0.rs
+++ src/chip8/instructions/op_0.rs
@@ -1,12 +1,24 @@
-use crate::chip8::{ Cpu, Instruction };
+use crate::chip8::{Cpu, Instruction, DISPLAY_DIMENSIONS, PIXEL_OFF};
 
 impl Cpu {
     /// Contains 2 instructions:
-    /// 0x00EE/RET -> return from a subroutine.
     /// 0x00E0/CLR -> clear the display.
+    /// 0x00EE/RET -> return from a subroutine.
     pub fn op_0(&mut self, ins: Instruction) {
         if ins.x == 0 {
-            
+            match ins.kk {
+                // CLR
+                0x0E => {
+                    self.display_memory = [PIXEL_OFF; DISPLAY_DIMENSIONS]
+                }
+
+                // RET
+                0xEE => {
+                    if self.stack.pop().is_none() { panic!("Stack Underflow") }
+                }
+
+                _ => ()
+            }
         }
     }
 
