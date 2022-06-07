4d88c7b6efa45b4d252c27fda0b81e0da83976aa
G. Morris <93727813+v-maxson@users.noreply.github.com>
2022-06-06 22:47:11 -0600

    style(format): fix comment format

--- src/chip8/instructions/op_0.rs
+++ src/chip8/instructions/op_0.rs
@@ -2,8 +2,8 @@ use crate::chip8::{Cpu, Instruction, DISPLAY_DIMENSIONS, PIXEL_OFF};
 
 impl Cpu {
     /// Contains 2 instructions:
-    /// 0x00E0/CLR -> clear the display.
-    /// 0x00EE/RET -> return from a subroutine.
+    /// - 0x00E0/CLR -> clear the display.
+    /// - 0x00EE/RET -> return from a subroutine.
     pub fn op_0(&mut self, ins: Instruction) {
         if ins.x == 0 {
             match ins.kk {
