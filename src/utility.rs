2b2f1350e6a43ef80660a8859e8a63284a945952
G. Morris <93727813+v-maxson@users.noreply.github.com>
2022-06-07 19:53:05 -0600

    feat: add get_bit_at and get_bit_arr_u8 functions

--- src/utility.rs
+++ src/utility.rs
@@ -1,4 +1,5 @@
-//! Contains miscellaneous functionality. 
+//! Contains miscellaneous functionality.
+#![allow(unused)]
 
 use crate::error;
 
@@ -14,4 +15,24 @@ pub fn hook_panic() {
             error!("An unhandled runtime exception occured, please open an issue on the Github repository (found here: https://github.com/v-maxson/Memu).");
         }
     }));
+}
+
+/// Returns the value of the bit at n.
+pub fn get_bit_at(input: u8, n: u8) -> u8 {
+    if n < 8 {
+        input & (1 << n)
+    } else {
+        0
+    }
+}
+
+/// Returns a array of each bit.
+pub fn get_bit_arr_u8(number: u8) -> [bool; 8] {
+    let mut bitarr = [false; 8];
+
+    for (i, bit) in bitarr.iter_mut().enumerate() {
+        *bit = number & (0x80 >> i) as u8 > 0;
+    }
+    
+    bitarr
 }
