79721ed919fb8ed8fa9a312cacb4bfdc67817c6d
G. Morris <93727813+v-maxson@users.noreply.github.com>
2022-06-10 17:51:51 -0600

    feat: add test rom to builtins

--- src/chip8/builtins.rs
+++ src/chip8/builtins.rs
@@ -11,6 +11,7 @@ pub static BUILTINS: BuiltinsMap = {
     let mut map = BuiltinsMap::default();
 
     map.insert("ibm", include_bytes!("../../resources/chip8/IBM.ch8"));
+    map.insert("test", include_bytes!("../../resources/chip8/test.ch8"));
 
     map
 };
