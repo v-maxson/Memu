bd2173f85a702701c832ee6ba3dd024a7a9a23f2
G. Morris <93727813+v-maxson@users.noreply.github.com>
2022-06-13 11:58:04 -0600

    refactor: clean up instruction struct

--- src/chip8/instruction.rs
+++ src/chip8/instruction.rs
@@ -3,39 +3,26 @@
 use bitfield::bitfield;
 
 bitfield! {
+    /// Represents a single CHIP-8 instruction. 
     #[derive(Copy, Clone)]
-    struct __OPXYN(u16);
-    n, _: 3, 0;
-    y, _: 7, 4;
-    x, _: 11, 8;
-    op, _: 15, 12;
-}
+    pub struct Instruction(u16);
 
-bitfield! {
-    #[derive(Copy, Clone)]
-    struct __JJKK(u16);
-    kk, set_kk: 7, 0;
-    jj, set_jj: 15, 8;
-}
+    u16;
+    pub full, set_full: 15, 0;
+    pub nnn, set_nnn: 11, 0;
 
-bitfield! {
-    #[derive(Copy, Clone)]
-    struct __NNN(u16);
-    nnn, _: 11, 0;
-}
-
-/// Represents a single CHIP-8 instruction. 
-#[derive(Copy, Clone)]
-pub union Instruction {
-    full: u16,
-    opxyn: __OPXYN,
-    jjkk: __JJKK,
-    nnn: __NNN
+    u8;
+    pub op, set_op: 15, 12;
+    pub x, set_x: 11, 8;
+    pub y, set_y: 7, 4;
+    pub n, set_n: 3, 0;
+    pub jj, set_jj: 15, 8;
+    pub kk, set_kk: 7, 0;
 }
 
 impl From<u16> for Instruction {
     fn from(value: u16) -> Self {
-        Self { full: value }
+        Self(value)
     }
 }
 
@@ -48,50 +35,11 @@ impl From<Instruction> for u16 {
 impl Instruction {
     pub fn from_u8_pair(high: u8, low: u8) -> Self {
         unsafe {
-            let mut new = Self { full: 0x0000 };
-            new.jjkk.set_jj(high as u16);
-            new.jjkk.set_kk(low as u16);
+            let mut new = Self(0x0000);
+            new.set_jj(high);
+            new.set_kk(low);
             new
         }
-    }
-
-    pub fn full(&self) -> u16 {
-        unsafe {self.full}
-    }
-
-    #[inline(always)]
-    pub fn op(&self) -> u8 {
-        unsafe {self.opxyn.op() as u8}
-    }
-
-    #[inline(always)]
-    pub fn x(&self) -> u8 {
-        unsafe {self.opxyn.x() as u8}
-    }
-
-    #[inline(always)]
-    pub fn y(&self) -> u8 {
-        unsafe {self.opxyn.y() as u8}
-    }
-
-    #[inline(always)]
-    pub fn n(&self) -> u8 {
-        unsafe {self.opxyn.n() as u8}
-    }
-
-    #[inline(always)]
-    pub fn jj(&self) -> u8 {
-        unsafe {self.jjkk.jj() as u8}
-    }
-
-    #[inline(always)]
-    pub fn kk(&self) -> u8 {
-        unsafe {self.jjkk.kk() as u8}
-    }
-
-    #[inline(always)]
-    pub fn nnn(&self) -> u16 {
-        unsafe {self.nnn.nnn()}
     }
 }
 
