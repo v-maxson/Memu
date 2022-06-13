17ae4a1ca18d9599c2b4dac3642e83a9af985189
G. Morris <93727813+v-maxson@users.noreply.github.com>
2022-06-13 16:13:55 -0600

    revert: undo 057b644

--- src/chip8/instructions/op_8.rs
+++ src/chip8/instructions/op_8.rs
@@ -1,13 +1,4 @@
-use crate::chip8::{Cpu, Instruction};
-
-/// Returns the value of the bit at n.
-pub fn get_bit_at(input: u8, n: u8) -> u8 {
-    if n < 8 {
-        input & (1 << n)
-    } else {
-        0
-    }
-}
+use crate::chip8::{Cpu, Instruction, get_bit_at};
 
 impl Cpu {
     /// Contains 8 instructions:
