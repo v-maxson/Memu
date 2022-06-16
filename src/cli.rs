afddf20873bf3e9dad87c79ef29dc74be89be0bc
G. Morris <93727813+v-maxson@users.noreply.github.com>
2022-06-15 22:32:27 -0600

    feat: add list flag to chip8

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,9 @@
 //! Contains the logic for the CLI.
-#![allow(unused, unreachable_patterns)]
+#![allow(unused, unreachable_patterns, unstable_name_collisions)]
 
 use clap::{Parser, Subcommand};
-use crate::{error, chip8};
+use crate::{error, chip8::{self, BUILTINS}, info};
+use itertools::Itertools;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -17,7 +18,7 @@ enum Command {
     /// The CHIP-8 emulator.
     Chip {
         /// The ROM to execute.
-        #[clap(short, long)]
+        #[clap(short, long, default_value = "")]
         rom: String,
 
         /// The CPU speed (in hz) 
@@ -26,7 +27,11 @@ enum Command {
 
         /// The scale of the display.
         #[clap(short = 's', long = "scale", default_value_t = 16)]
-        display_scale: u8
+        display_scale: u8,
+
+        /// Prints a list of built-in ROMs and exits.
+        #[clap(short)]
+        list: bool
     }
 }
 
@@ -35,9 +40,17 @@ pub fn run() {
     let args = CliArguments::parse();
 
     match args.command {
-        Command::Chip { rom, clock_speed, display_scale } => {
-            if clock_speed > 1000 { error!("clock_speed cannot be greater than 1000"); return; }
-            if display_scale > 32 { error!("display_scale cannot be greater than 32"); return; }
+        Command::Chip { rom, clock_speed, display_scale, list } => {
+            if list {
+                let builtin_list: String = BUILTINS.iter().map(|c| format!("\t- {}", c.0)).intersperse(String::from("\n")).collect();
+
+                info!("CHIP-8 Built-in ROMs:\n{}", builtin_list);
+                return;
+            }
+
+            if rom.is_empty() { error!("A ROM (--rom <rom_path or builtin>) must be provided."); return; }
+            if clock_speed > 1000 { error!("clock_speed cannot be greater than 1000."); return; }
+            if display_scale > 32 { error!("display_scale cannot be greater than 32."); return; }
 
             chip8::Cpu::start(rom.as_str(), clock_speed as f64, display_scale);
         },
